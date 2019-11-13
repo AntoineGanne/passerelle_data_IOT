2755f59aa29f329e3507f87214764ae419d497ad
Antoine <antoine.ganne@hotmail.fr>
2019-11-13 17:47:04 +0100

    message broker

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 passerelle_data_IOT
+
+
+infos a stocker en base:
+* id  (auto-increment)
+* timestamp
+* id_compteur
+* puissance apparente
+* conso compteur totale
+* politique tarifaire (Tarif en cours)
+
+
+
+
+
+http://192.168.20.104/api/xdevices.json?cmd=10
+
+https://www.hivemq.com/blog/mqtt-client-library-enyclopedia-hivemq-mqtt-client/
--- src/main/java/Client_compteur.kt
+++ src/main/java/Client_compteur.kt
@@ -6,6 +6,11 @@ class Client_compteur {
     private val API_URL_XML = "http://$host/protect/settings/teleinfo2.xml"
     private val API_URL_JSON = "http://$host/api/xdevices.json?cmd=10"
 
+    // valeurs mocked
+    var mock_puissance_apparente= 90
+    var mock_compteur_conso_totale=1876152
+    var mock_json = JSONObject("{\"product\":\"Eco-devices\",\"T1_PTEC\":\"TH..\",\"T1_PAPP\":90,\"T1_BASE\":1876058,\"T2_PTEC\":\"----\",\"T2_PAPP\":0,\"T2_BASE\":0,\"INDEX_C1\":784659,\"INDEX_C2\":11280}")
+
 
     fun getInfos(): Pair<Long, Long> {
         var infos = JSONObject(URL(API_URL_JSON).readText())
@@ -13,6 +18,7 @@ class Client_compteur {
 
         var compteur_conso_totale=infos.get("T1_BASE").toString() // en watt/h
         println(puissance_apparente)
+
         println(compteur_conso_totale)
         return Pair(puissance_apparente.toLong(),compteur_conso_totale.toLong())
     }
